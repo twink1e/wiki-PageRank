bc1b0fd3cf05b594d0c600d64255ba03073985ad
twink1e <tkgs.si.junke@gmail.com>
2017-11-12 21:20:47 +0800

    add index links

--- DataProcessor.java
+++ DataProcessor.java
@@ -7,6 +7,7 @@ class DataProcessor {
   private static final String RAW = "indexbi.bin", LINK = "links.bin", OFFSET = "offset.bin";
   private static HashMap<Integer, Integer> off2idx;
   private static long start, end;
+  private static int pageNum;
 
   private static int getLittleEndian(byte[] b) {
     return ((b[3]&0xff)<<24)+((b[2]&0xff)<<16)+((b[1]&0xff)<<8)+(b[0]&0xff);
@@ -24,7 +25,7 @@ class DataProcessor {
       byte[] buffer = new byte[4];
       mbb.position(4); //skip version number
       mbb.get(buffer);
-      int pageNum = getLittleEndian(buffer);
+      pageNum = getLittleEndian(buffer);
       int offset = 16; //first page
       int linkNum;
 
@@ -35,9 +36,8 @@ class DataProcessor {
         mbb.get(buffer); 
         linkNum = getLittleEndian(buffer);
         offset += (4 + linkNum) * 4; // 4 ints of meta data + links
-        System.out.println(i);
       }
-      if (offset == in.size()) System.out.println("Map offset success!");
+      if (offset == in.size()) System.out.println("Map offset success!"); // check if offset tallies
       else System.out.println("Map offset failed " + offset);
       in.close();
       fos.close();
@@ -45,35 +45,43 @@ class DataProcessor {
       System.out.println(e);
     }
   }
-  private static void indexPages() {
+
+  private static void indexLinks() {
     try {
-      FileInputStream fis = new FileInputStream(RAW);
-      BufferedInputStream bis = new BufferedInputStream(fis);
-      DataInputStream dis = new DataInputStream(bis);
+      FileChannel in = new FileInputStream(RAW).getChannel();
+      MappedByteBuffer mbb = in.map(FileChannel.MapMode.READ_ONLY, 0, in.size());
+
       FileOutputStream fos = new FileOutputStream(LINK);
       BufferedOutputStream bos = new BufferedOutputStream(fos);
       DataOutputStream dos = new DataOutputStream(bos);
+
+      dos.writeInt(pageNum);
+      mbb.position(16); //start of first page
       byte[] buffer = new byte[4];
-      int num;
-      int count = 0;
-      while (dis.available() > 0) {
-        dis.read(buffer);
-        num = getLittleEndian(buffer);
-        if (num == 0) {
-          num = count++;
-          //System.out.println(count);
+      int linkNum, offset, idx;
+
+      for (int i=0; i<pageNum; i++) {
+        dos.writeInt(i);
+        mbb.position(mbb.position() + 4); // skip the 0
+        mbb.get(buffer); 
+        linkNum = getLittleEndian(buffer);
+        dos.writeInt(linkNum);
+        mbb.position(mbb.position() + 8); // skip B & M
+
+        for (int j=0; j<linkNum; j++) {
+          mbb.get(buffer); 
+          offset = getLittleEndian(buffer);
+          idx = off2idx.get(offset);
+          dos.writeInt(idx);
         }
-        dos.writeInt(num);
-        //System.out.println(num);
       }
-      dis.close();
-      dos.close();
-      if (count == 8166507) System.out.println("Success!");
-      else System.out.println("Fail! " + count);
+      if (mbb.position() == in.size()) System.out.println("Index links success!");
+      else System.out.println("Index links failed " + mbb.position());
+      in.close();
+      fos.close();
     } catch (Exception e) {
       System.out.println(e);
     }
-
   }
 
   private static void readBinaryBuffered(String file, int num) {
@@ -104,5 +112,9 @@ class DataProcessor {
     mapOffset2Idx();
     end = System.currentTimeMillis();
     System.out.println("Map offset took "+ (end-start) + " ms");
+    start = System.currentTimeMillis();
+    indexLinks();
+    end = System.currentTimeMillis();
+    System.out.println("Index links took "+ (end-start) + " ms");
   }
 }
